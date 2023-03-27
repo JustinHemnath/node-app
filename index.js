9855bf4551ce9f9e7b4ab38e17c497286a4efbab
Justin Hemnath <justin.hemnath.96@gmail.com>
2023-03-27 22:41:48 +0530

    mongoose connection

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+import { connectDB } from './src/db/mongodb.js';
 
-// const app = express();
-// const port = process.env.port || 3001;
+dotenv.config();
 
-// app.use(express.json());
+const app = express();
+const port = process.env.port || 3001;
 
-// app.get('', (req, res) => {
-//     res.send('<h1>HELLO</h1>');
-// });
+app.use(express.json());
 
-// app.get('/about', (req, res) => {
-//     console.log(req.body);
-//     res.send(`Hello`);
-// });
+app.get('', (req, res) => {
+    res.send('<h1>HELLO</h1>');
+});
+
+app.get('/about', (req, res) => {
+    console.log(req.body);
+    res.send(`Hello`);
+});
 
 // app.listen(port, () => {
 //     console.log('Server up and running');
 // });
 
-import mongoose from 'mongoose';
-
-mongoose.connect(
-    'mongodb+srv://justinhemnath96:Solicitarmunicion@123@cluster0.jjzotxs.mongodb.net/?retryWrites=true&w=majority',
-    { useNewUrlParser: true }
-);
+(async function startServer() {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            console.log('Server up and running');
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})();
